d86e3e29b2b5be2ebd5419d9298f5f20f6b636c1
Lisa W <wan.lis@northeastern.edu>
2025-09-15 02:34:42 -0400

    Initial Commit: Dashboard Set Up

---
+++ index.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 1400,
+        height: 900,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    mainWindow.loadFile('index.html');
+    
+    // Open DevTools in development
+    if (process.env.NODE_ENV === 'development') {
+        require('electron-reload')(__dirname, {
+            electron: require('electron'),
+            hardResetMethod: 'exit'
+        });
+    }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
